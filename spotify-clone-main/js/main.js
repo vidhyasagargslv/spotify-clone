d8de4471a5e1c9a7a48cfef0e930c0485681aeac
vidhyasagargslv <103246644+vidhyasagargslv@users.noreply.github.com>
2022-10-29 17:52:48 +0530

    Update main.js

--- spotify-clone-main/js/main.js
+++ spotify-clone-main/js/main.js
@@ -1,13 +1,13 @@
-//Navbar toggle button
+     //Navbar toggle button
 function toggler(x) {
     x.classList.toggle("change");
     var sidenav = document.getElementById("mySidenav");
     var opacity = document.getElementById("opacity");
     sidenav.classList.toggle("open");
     opacity.classList.toggle("opa");
-    document.body.classList.toggle("opa");
+       document.body.classList.toggle("opa");
 }
-//Profile button
+     //Profile button
 function profile() {
     var profileMenu = document.getElementById("profileMenu");
     profileMenu.classList.toggle("active");
